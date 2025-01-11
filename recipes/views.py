b35eb2f8f3934d77d760e4d6c0d6c24a4d4eff23
lslucaso <lslemos59@gmail.com>
2025-01-11 18:18:28 -0300

    #

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html')
+    
+    context = {
+        'name': 'Lucas Santana'
+    }
+    
+    return render(request, 'recipes/home.html', context)
