22886c2465c242a11618587b3403e685ccc63244
lslucaso <lslemos59@gmail.com>
2025-02-01 12:36:35 -0300

    #

--- projeto/urls.py
+++ projeto/urls.py
@@ -1,5 +1,7 @@
 
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 from django.urls import path, include
 
 
@@ -7,3 +9,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('recipes.urls'))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
