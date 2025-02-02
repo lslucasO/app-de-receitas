441bbd172c70733f076c0d84bd45f0eb5a89d3e7
lslucaso <lslemos59@gmail.com>
2025-02-02 15:06:20 -0300

    #

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_list_or_404, render
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 from utils.recipes.factory import make_recipe
 
 from recipes.models import Recipe
@@ -28,11 +28,12 @@ def category(request, category_id):
 
 
 def recipe(request, id):
-    recipe = Recipe.objects.filter(
+    recipe = get_object_or_404(
+        Recipe,
         pk=id,
         is_published=True,
-    ).order_by('-id').first()
-
+    )
+    
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': recipe,
         'is_detail_page': True,
