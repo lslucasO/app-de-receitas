4429b5707f76d91b086c0cd4bb2c8963fb06d50e
lslucaso <lslemos59@gmail.com>
2025-02-02 22:20:19 -0300

    #

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
-from django.urls import reverse
+from django.urls import reverse, resolve
+from recipes import views
 
 
 class RecipeURLsTest(TestCase):
@@ -18,3 +19,21 @@ class RecipeURLsTest(TestCase):
         self.assertEqual(url, '/recipes/1/')
         
         
+class RecipeViewsTest(TestCase):
+    def test_recipe_home_view_function_is_correct(self):
+        view = resolve(reverse('recipes:home'))
+        self.assertIs(view.func, views.home) 
+        
+        
+    def test_recipe_category_view_function_is_correct(self):
+        view = resolve(
+            reverse('recipes:category', kwargs={'category_id': 1})
+        )
+        self.assertIs(view.func, views.category) 
+        
+        
+    def test_recipe_detail_view_function_is_correct(self):
+        view = resolve(
+            reverse('recipes:recipe', kwargs={'id': 1})               
+        )
+        self.assertIs(view.func, views.recipe) 
