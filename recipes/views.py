e9ed7794c9dc45d842d278da83c685e4f9702bdb
lslucaso <lslemos59@gmail.com>
2025-02-25 20:50:18 -0300

    #

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 
 from recipes.models import Recipe
@@ -34,3 +35,11 @@ def recipe(request, id):
         'recipe': recipe,
         'is_detail_page': True,
     })
+    
+def search(request):
+    search_term = request.GET.get('q')
+
+    if not search_term:
+        raise Http404()
+
+    return render(request, 'recipes/pages/search.html')
