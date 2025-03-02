30dda3daf6158275a5aed72cc77f25bca9570517
lslucaso <lslemos59@gmail.com>
2025-03-02 13:24:04 -0300

    #

--- authors/views.py
+++ authors/views.py
@@ -1,15 +1,22 @@
+from django.http import Http404
 from django.shortcuts import render
 
 from .forms import RegisterForm
 
 
 def register_view(request):
-    
-    if request.POST:
-        form = RegisterForm(request.POST)
-    else:
-        form = RegisterForm()
+    form = RegisterForm()
         
+    return render(request, 'authors/pages/register_view.html', {
+        'form': form
+    })
+
+def register_create(request):
+    if not request.POST:
+        raise Http404()
+
+    form = RegisterForm(request.POST)
+    
     return render(request, 'authors/pages/register_view.html', {
         'form': form
     })
