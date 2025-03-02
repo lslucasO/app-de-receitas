b11b82f8d60220234e4b617f8f2603a7659797e3
lslucaso <lslemos59@gmail.com>
2025-03-02 12:01:35 -0300

    #

--- project/settings.py
+++ project/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from django.contrib.messages import constants
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -8,10 +9,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jmoc5%*br=r10a)3+xi*wm2xf$+zhubwowq*-2v=s(5vg4o76u'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'INSECURE')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if os.environ.get('DEBUG') == 1 else False
 
 ALLOWED_HOSTS = []
 
@@ -121,3 +122,11 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MESSAGE_TAGS = {
+    constants.DEBUG: 'message-debug',
+    constants.ERROR: 'message-error',
+    constants.INFO: 'message-info',
+    constants.SUCCESS: 'message-success',
+    constants.WARNING: 'message-warning',
+}
--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,11 @@
 from django.db.models import Q
 from django.http.response import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
+from django.contrib import messages
 from utils.pagination import make_pagination
 
 from recipes.models import Recipe
+
 import os
 
 PER_PAGE = os.environ.get('PER_PAGE', 6)
@@ -12,9 +14,11 @@ def home(request):
     recipes = Recipe.objects.filter(
         is_published=True,
     ).order_by('-id')
+    
+    messages.success(request, 'Epa, você foi pesquisar algo que eu vi.')
 
     page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
-
+    
     return render(request, 'recipes/pages/home.html', context={
         'recipes': page_obj,
         'pagination_range': pagination_range
