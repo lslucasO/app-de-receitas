efb7280eb783a46bf3a11121d892b01d5aaa894f
lslucaso <lslemos59@gmail.com>
2025-03-02 13:11:17 -0300

    #

--- authors/views.py
+++ authors/views.py
@@ -4,7 +4,12 @@ from .forms import RegisterForm
 
 
 def register_view(request):
-    form = RegisterForm()
+    
+    if request.POST:
+        form = RegisterForm(request.POST)
+    else:
+        form = RegisterForm()
+        
     return render(request, 'authors/pages/register_view.html', {
         'form': form
     })
