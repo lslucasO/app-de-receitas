6b453168601f069582aef0d8a5d4d1dca642a55c
lslucaso <lslemos59@gmail.com>
2025-03-02 21:34:26 -0300

    #

--- authors/views.py
+++ authors/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.http import Http404
 from django.shortcuts import redirect, render
 
@@ -7,7 +8,6 @@ from .forms import RegisterForm
 def register_view(request):
     register_form_data = request.session.get('register_form_data', None)
     form = RegisterForm(register_form_data)
-    
     return render(request, 'authors/pages/register_view.html', {
         'form': form,
     })
@@ -20,5 +20,11 @@ def register_create(request):
     POST = request.POST
     request.session['register_form_data'] = POST
     form = RegisterForm(POST)
+
+    if form.is_valid():
+        form.save()
+        messages.success(request, 'Your user is created, please log in.')
+
+        del(request.session['register_form_data'])
 
     return redirect('authors:register')
