4ee9868cd0bf352bf7b4dbd741de393f6a9fb3bd
lslucaso <lslemos59@gmail.com>
2025-03-05 13:13:28 -0300

    #

--- recipes/views.py
+++ recipes/views.py
@@ -1,22 +1,50 @@
+import os
+from typing import List
+
 from django.db.models import Q
 from django.http.response import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
+from django.views.generic import ListView
 from utils.pagination import make_pagination
 
 from recipes.models import Recipe
 
-import os
+PER_PAGE = int(os.environ.get('PER_PAGE', 6))
 
-PER_PAGE = os.environ.get('PER_PAGE', 6)
+
+class RecipeListViewBase(ListView):
+    model = Recipe
+    context_object_name = 'recipes'
+    ordering = ['-id']
+    template_name = 'recipes/pages/home.html'
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super().get_queryset(*args, **kwargs)
+        qs = qs.filter(
+            is_published=True,
+        )
+        return qs
+
+    def get_context_data(self, *args, **kwargs):
+        ctx = super().get_context_data(*args, **kwargs)
+        page_obj, pagination_range = make_pagination(
+            self.request,
+            ctx.get('recipes'),
+            PER_PAGE
+        )
+        ctx.update(
+            {'recipes': page_obj, 'pagination_range': pagination_range}
+        )
+        return ctx
+
 
 def home(request):
     recipes = Recipe.objects.filter(
         is_published=True,
     ).order_by('-id')
-    
 
     page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
-    
+
     return render(request, 'recipes/pages/home.html', context={
         'recipes': page_obj,
         'pagination_range': pagination_range
